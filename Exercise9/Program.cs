41962ceb41e5333c7d46fc35717520cfe53dd844
Tyler C <tyler.crumb@s.capregboces.org>
2024-03-05 10:13:02 -0500

    Exercise 9 Part 1 completed

---
+++ Exercise9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose the row width");
+                int width = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("how many rows");
+                int rows = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
